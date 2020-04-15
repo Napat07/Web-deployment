7d29ac3a79e136632ba33b2eb287c016d22a5271
Napat07 <naphat1811@gmail.com>
2020-04-15 22:41:00 +0700

    add Crash

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ app.get('/',(req,res) => {
     res.send('Hello world')
 })
 
+app.get('/crash',(req,res) => {
+    console.log('Crash !!!')
+    res.send('Crash')
+    process.exit(1)
+})
+
 app.listen(80, () => console.log('Server in running at ', 80))
